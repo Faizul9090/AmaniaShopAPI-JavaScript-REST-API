f4b053f60ba7f91846454925a3826e05ae89a5a5
Faizul9090 <15203120@iubat.edu>
2021-01-03 01:04:15 +0600

    Added patch() methode to index.js for call update() with partial data.

--- AmaniaShopAPI/index.js
+++ AmaniaShopAPI/index.js
@@ -167,6 +167,37 @@ router.delete('/:id', function(req, res, next){
     });
 });
 
+///Added patch() methode to index.js for call update() with partial data.
+/// http://localhost:5000/api/5
+router.patch('/:id', function(req, res, next){
+    pieRepo.getById(req.params.id, function(data){
+        if(data){
+            // Attempt to delete the data
+            pieRepo.update(req.body, req.params.id, function(data){
+                res.status(200).json({
+                    "status": 200,
+                    "statusText": "OK",
+                    "message": "The pie '" + req.params.id + "' is patched.",
+                    "data": data
+                });
+            });
+        }
+        else{
+            res.status(404).json({
+                "status": 404,
+                "statusText": "Not Found",
+                "message": "The Pie '" + req.params.id + "' could not be found.",
+                "error": {
+                    "code": "Not_Found",
+                    "message": "The pie '" + req.params.id + "' could not be found."
+                }
+            });
+        }
+    }, function(err){
+        next(err);
+    });
+});
+
 // Configur router so all routers are prefixed with /api/v1
 app.use('/api/',router);
 
