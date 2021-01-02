99db0e07ac8016ffedf7e723ca748159dd6e292e
Faizul9090 <15203120@iubat.edu>
2021-01-02 10:45:31 +0600

    complete "Retrieve and Search for Data Using REST API Methods"

--- AmaniaShopAPI/index.js
+++ AmaniaShopAPI/index.js
@@ -11,17 +11,70 @@ let router = express.Router();
 //     {"id": 2, "name": "Cherry"},
 //     {"id": 3, "name": "Peach"}
 // ];
-let pies = pieRepo.get(); 
+
+//let pies = pieRepo.get();
+
 //Create GET to return a list of all pies
 router.get('/', function(reg, res, next){
     // res.send("Welcome to Amania Shop 'Faizul', New Product is Apple");
-    res.status(200).json({
-        "status": 200,
-        "statusText": "ok",
-        "message" : "All pies retrieved.",
-        "data" : pies
+    pieRepo.get(function(data){
+        res.status(200).json({
+            "status": 200,
+            "statusText": "ok",
+            "message" : "All pies retrieved.",
+            "data" : data
+        });
+    },function(err){
+        next(err);
     });
-    send(pies);
+});
+
+///Create GET/search?id=n&name=str to search for pies by 'id' and/or 'name' pie
+///http://localhost:5000/api/search?id=1&name=A
+///http://localhost:5000/api/search/?id=1&name=A
+///http://localhost:5000/api/search?name=A
+router.get('/search', function(req, res, next){
+    let searchObject = {
+        "id": req.query.id,
+        "name": req.query.name
+    };
+    pieRepo.search(searchObject, function(data){
+        res.status(200).json({
+            "status": 200,
+            "statusText": "OK",
+            "message": "All pies retrieved.",
+            "data": data
+        });
+    }, function(err){
+        next(err);
+    });
+});
+
+///Create GET/ID to return a single pie
+router.get('/:id', function(req, res,nest){
+    pieRepo.getById(req.params.id, function(data){
+        if(data){
+            res.status(200).json({
+                "status": 200,
+                "statusText": "OK",
+                "message": "Single pie retrieved.",
+                "data": data
+            });
+        }
+        else{
+            res.status(404).json({
+                "status": 404,
+                "statusText": "Not Found",
+                "message": "The pie '" + req.params.id + "' Could not found.",
+                "error": {
+                    "code": "NOT_FOUND",
+                    "message": "The pie '" + req.params.id + "' could not be found."
+                }
+            });
+        }
+    }, function(err){
+        next(err);
+    });
 });
 
 // Configur router so all routers are prefixed with /api/v1
--- AmaniaShopAPI/repos/pieRepo.js
+++ AmaniaShopAPI/repos/pieRepo.js
@@ -1,11 +1,62 @@
+/// "fs" buildin node module which is work with reading and writing fiels
+let fs = require('fs');
+
+const FILE_NAME = './assets/pies.json';
+
 let pieRepo = {
-    get: function(){
-        return [
-            {"id": 1, "name": "Apple"},
-            {"id": 2, "name": "Cherry"},
-            {"id": 3, "name": "Peach"}
-        ];
+    get: function(resolve, reject){
+        fs.readFile(FILE_NAME, function(err, data){
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(JSON.parse(data));
+            }
+        });
+    }, /// added new function for Get Data by Id
+    getById: function(id, resolve, reject){
+        fs.readFile(FILE_NAME, function(err, data){
+            if(err){
+                reject(err);
+            }
+            else{
+                let pie = JSON.parse(data).find(p => p.id == id);
+                resolve(pie);
+            }
+        });
+    },
+    search: function(searchObject, resolve, reject){
+        fs.readFile(FILE_NAME, function(err, data){
+            if(err){
+                reject(err);
+            }
+            else{
+                let pies =JSON.parse(data);
+                // Perform search
+                if(searchObject){
+                    //Example search object
+                    // let searchObject = {
+                    //      "id": 1,
+                    //      "name": 'A'
+                    //};
+                    pies = pies.filter(
+                        p => (searchObject.id ? p.id == searchObject.id : true) &&
+                            (searchObject.name ? p.name.toLowerCase().indexOf(searchObject.name.toLowerCase()) >= 0 : true));
+                }
+                resolve(pies);
+            }
+        });
     }
-} 
+};
+
+// let pieRepo = {
+//     get: function(){
+//         return [
+//             {"id": 1, "name": "Apple"},
+//             {"id": 2, "name": "Cherry"},
+//             {"id": 3, "name": "Peach"}
+//         ];
+//     }
+// } 
 
 module.exports = pieRepo;
